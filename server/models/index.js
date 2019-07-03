489c571fb4c422f41ce7858784fc67bf98eb6822
Otto Hu <ottohu101@gmail.com>
2019-07-03 01:44:29 -0700

    testing with sequelize

--- server/models/index.js
+++ server/models/index.js
@@ -14,7 +14,15 @@ if (config.use_env_variable) {
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
-
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+  
 fs
   .readdirSync(__dirname)
   .filter(file => {
