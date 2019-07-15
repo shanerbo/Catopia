815f5180dff0c7b5f1e07f1b21e867bcedfdc425
Otto Hu <ottohu101@gmail.com>
2019-07-14 20:55:15 -0700

    setup angular for dev

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { AppRoutingModule } from './app-routing.module';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PhotoCardComponent } from './photo-card/photo-card.component';
+import { LeftPanelComponent } from './left-panel/left-panel.component';
+import { RightPanelComponent } from './right-panel/right-panel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    NavBarComponent,
+    PhotoCardComponent,
+    LeftPanelComponent,
+    RightPanelComponent
   ],
   imports: [
     BrowserModule,
