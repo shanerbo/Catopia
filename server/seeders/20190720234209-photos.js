ef25db2b9dcbaad8e2db2722e03347bebe704a68
erbo <erbo_shan@sfu.ca>
2019-07-20 16:59:32 -0700

    add missed cats' name

---
+++ server/seeders/20190720234209-photos.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users',
+      [
+        {
+
+        }
+      ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+    return queryInterface.bulkDelete('Users', null, {});
+  }
+};
