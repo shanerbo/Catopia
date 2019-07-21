4d3ee92e4649bc50bf217a4d8566a3b86c3c0615
Otto Hu <ohu@sfu.ca>
2019-07-21 02:28:17 -0700

    quick fix and more progress with file upload

--- server/controllers/postsController.js
+++ server/controllers/postsController.js
@@ -1,13 +1,26 @@
 const User = require('../models/index').Users;
 const Post = require('../models/index').Posts;
+const gcp = require('../lib/google-cloud-storage');
 const { sanitizeBody, body, validationResult } = require('express-validator');
-const jwtsecret = process.env.JWTSECRET;
 const passport = require('passport');
 
 /*
-* @param 
+* @param: 
+*   - an array of image files in req.body.photos
+*   - a description
+*        
 */
 exports.postPhoto = [
-	passport.authenticate('jwt', { session: false }),
-	sanitizeBody('description').escape(),
+  // body(''),
+  sanitizeBody('description').escape(),
+  passport.authenticate('jwt', { session: false }),
+  (req, res, next) => {
+    console.log(req.body.description);
+    if (req.user) {
+      res.json({ upload: "success!" });
+
+    } else {
+      res.json({ error: "Needs login to upload photos" });
+    }
+  }
 ];
