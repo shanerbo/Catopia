be590451f1a03eb704fcee70c14236815ffefc7f
mca176 <mca176@sfu.ca>
2019-07-27 12:41:34 -0700

    remove useless stuff

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,26 +1,22 @@
 // External modules
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 // Our own stuff ❤
-import { HomePageComponent } from './home-page/home-page.component';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { PhotoCardComponent } from './photo-card/photo-card.component';
-import { LeftPanelComponent } from './left-panel/left-panel.component';
-import { RightPanelComponent } from './right-panel/right-panel.component';
-import { LoginComponent } from './login/login.component';
-import { LoginService } from './services/login.service';
-import { PhotoService } from './services/photo.service';
-import { UserProfileComponent } from './user-profile/user-profile.component';
-import { ComposeBoxComponent } from './compose-box/compose-box.component';
-import { PhotoInputComponent } from './photo-input/photo-input.component';
-
-
-
+import { HomePageComponent } from "./home-page/home-page.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { PhotoCardComponent } from "./photo-card/photo-card.component";
+import { LeftPanelComponent } from "./left-panel/left-panel.component";
+import { RightPanelComponent } from "./right-panel/right-panel.component";
+import { LoginComponent } from "./login/login.component";
+import { LoginService } from "./services/login.service";
+import { PhotoService } from "./services/photo.service";
+import { UserProfileComponent } from "./user-profile/user-profile.component";
+import { ComposeBoxComponent } from "./compose-box/compose-box.component";
 
 @NgModule({
   declarations: [
@@ -32,19 +28,10 @@ import { PhotoInputComponent } from './photo-input/photo-input.component';
     RightPanelComponent,
     LoginComponent,
     UserProfileComponent,
-    ComposeBoxComponent,
-    PhotoInputComponent
+    ComposeBoxComponent
   ],
-  imports: [
-    FormsModule,
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [
-    LoginService,
-    PhotoService
-  ],
+  imports: [FormsModule, BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [LoginService, PhotoService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
