477dca1991cd8eb75ee695cab1123c539165b871
mca176 <mca176@sfu.ca>
2019-07-27 15:28:18 -0700

    temp save

--- client/src/app/left-panel/left-panel.component.ts
+++ client/src/app/left-panel/left-panel.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from "@angular/core";
-
-import { SidebarBtnService } from "./left-panel-services/sidebar-btn.service";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-left-panel",
-  templateUrl: "./left-panel.component.html",
-  styleUrls: ["./left-panel.component.scss"]
+  selector: 'app-left-panel',
+  templateUrl: './left-panel.component.html',
+  styleUrls: ['./left-panel.component.scss']
 })
 export class LeftPanelComponent implements OnInit {
-  constructor(public siderbarbtn: SidebarBtnService) {}
-  isCollapsed: boolean = true;
+  constructor() {}
+  isCollapsed = true;
   toggleSideBar(): void {
     this.isCollapsed = !this.isCollapsed;
   }
