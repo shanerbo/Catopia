13ecc1f826ac4e85e7bf3fda5123dec56a85e25e
ohu <otto_hu@sfu.ca>
2019-07-27 15:04:46 -0700

    bugfix: compose box

--- client/src/app/compose-box/compose-box.component.ts
+++ client/src/app/compose-box/compose-box.component.ts
@@ -1,32 +1,30 @@
-import { Component, OnInit, EventEmitter, Output } from "@angular/core";
-import { Post } from "../interfaces/post";
-import { FormGroup, FormBuilder } from "@angular/forms";
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Post } from '../interfaces/post';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
-  selector: "app-compose-box",
-  templateUrl: "./compose-box.component.html",
-  styleUrls: ["./compose-box.component.scss"]
+  selector: 'app-compose-box',
+  templateUrl: './compose-box.component.html',
+  styleUrls: ['./compose-box.component.scss']
 })
 export class ComposeBoxComponent implements OnInit {
   @Output() post = new EventEmitter<FormData>();
 
   private photosToUpload: File[] = [];
-  public imgSrc: any[] = [];
-  public description = "";
-  private counter: number = 0;
-  constructor() {}
+  public imgSrc: Blob[] = [];
+  public description = '';
+  constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
   handleFileInput(files) {
     for (let i = 0; i < files.length; i++) {
       this.photosToUpload.push(files[i]);
-      this.counter++;
     }
 
-    for (let i = this.counter - files.length; i < this.counter; i++) {
-      var reader = new FileReader();
-      reader.readAsDataURL(this.photosToUpload[i]);
+    for (let i = 0; i < files.length; i++) {
+      const reader = new FileReader();
+      reader.readAsDataURL(files[i]);
       reader.onloadend = (event: any) => {
         console.log(event);
         this.imgSrc.push(event.target.result);
@@ -46,10 +44,10 @@ export class ComposeBoxComponent implements OnInit {
       return;
     }
     this.photosToUpload.forEach((photo, i) => {
-      formData.append("photo" + i, photo);
+      formData.append('photo' + i, photo);
     });
 
-    formData.append("description", this.description);
+    formData.append('description', this.description);
     this.post.next(formData);
   }
 }
