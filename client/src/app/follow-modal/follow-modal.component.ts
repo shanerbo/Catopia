2156b224e7d164ed16dff35ce63f3351e60051f7
jiaran <jiarany@163.com>
2019-07-27 22:57:12 -0700

    update html to angular following tab

--- client/src/app/follow-modal/follow-modal.component.ts
+++ client/src/app/follow-modal/follow-modal.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
+import { LoginService } from '../services/login.service';
 
 @Component({
   selector: 'app-follow-modal',
   templateUrl: './follow-modal.component.html',
   styleUrls: ['./follow-modal.component.scss']
 })
-export class FollowModalComponent implements OnInit {
+export class FollowModalComponent {
+  @Input() whichTab: string;
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+  loggedin = false;
+  constructor(public ls: LoginService) { }
+
+  // ngOnInit() {
+  // this.ls.currentUser.subscribe((user) => {
+  //   this.loggedin = !!user;
+  // });
+  // }
 
 }
--- client/src/app/user-profile/user-profile.component.ts
+++ client/src/app/user-profile/user-profile.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
+
 
 @Component({
   selector: 'app-user-profile',
+  template: `
+  <p id="follower-num" data-toggle="modal" data-target="#follower" style="color:rgba(5, 5, 5, 0.432);"
+                  (click)="switchTab()">
+                  <app-follow-modal>
+                  <h5 class="modal-title" id="exampleModalCenterTitle">this is {{whichTab}}</h5>
+                  </app-follow-modal>`
+  ,
+
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.scss']
 })
-export class UserProfileComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class UserProfileComponent {
+  public whichTab: string;
+  switchFollowing() {
+    this.whichTab = 'Following';
   }
+  switchFollower() {
+    this.whichTab = 'Follower';
+  }
+
 
 }
