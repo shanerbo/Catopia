22a23cc8373d40a8b1c5703aea520b860057aff6
jiaran <jiarany@163.com>
2019-07-28 00:59:11 -0700

    real data on recommend panel

--- client/src/app/right-panel/right-panel.component.ts
+++ client/src/app/right-panel/right-panel.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { UserInfo } from '../interfaces/user-info';
 
 @Component({
   selector: 'app-right-panel',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./right-panel.component.scss']
 })
 export class RightPanelComponent implements OnInit {
+  @Input() users: UserInfo;
 
   constructor() { }
 
   ngOnInit() {
+    console.log(this.users);
   }
 
 }
