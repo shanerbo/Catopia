30c36b62bbd95099b29124fbaf799f0216dbd03b
Otto Hu <ohu@sfu.ca>
2019-08-03 09:48:53 -0700

    making pages responsive

--- client/src/app/cat-profile/cat-profile.component.ts
+++ client/src/app/cat-profile/cat-profile.component.ts
@@ -1,17 +1,35 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChange } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-cat-profile',
   templateUrl: './cat-profile.component.html',
   styleUrls: ['./cat-profile.component.scss']
 })
-export class CatProfileComponent implements OnInit {
+export class CatProfileComponent implements OnInit, OnChanges {
   @Input() catInfo: any;
   @Input() ownerInfo: any;
+  public moment = moment;
+  public catAge;
+  public catAgeString;
   constructor(
 
   ) { }
 
+  ngOnChanges() {
+    if (this.catInfo) {
+      this.catAgeString = 'years';
+      this.catAge = Math.floor(moment().diff(this.catInfo.age, this.catAgeString));
+      if (this.catAge < 1) {
+        this.catAgeString = 'months';
+        this.catAge = Math.floor(moment().diff(this.catInfo.age, this.catAgeString));
+        if (this.catAge < 1) {
+          this.catAgeString = 'days';
+          this.catAge = Math.floor(moment().diff(this.catInfo.age, this.catAgeString));
+        }
+      }
+    }
+  }
   ngOnInit() {
 
   }
